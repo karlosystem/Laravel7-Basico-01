5b2894812fb628d2066502bb72ba3220c26c3d1b
Carlos Marquina <karlosystem@gmail>
2020-04-13 11:29:35 -0500

    este es mi primer commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Bienvenido a karlosystem
+
+Gracias por haberse registrado en mi pagina web
+
+
+Atentamente,<br>
+Carlos Marquina
+@endcomponent
---
+++ resources/views/sample.blade.php
@@ -0,0 +1,1 @@
+<h1>Hola Carlos Marquina</h1>
